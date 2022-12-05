9236a41a4c7f00e989e6654a2d3e289653313b7d
Calnoras <calnoras@gmail.com>
2022-12-05 18:57:07 +0100

    add barcode.read plugin

---
+++ src/Plugins/ZXing/Models/BarcodeFormats.cs
@@ -0,0 +1,29 @@
+namespace AyBorg.Plugins.ZXing.Models
+{
+        public enum BarcodeFormats
+        {
+                Undefined,
+                AZTEC,
+                CODABAR,
+                CODE_39,
+                CODE_93,
+                CODE_128,
+                DATA_MATRIX,
+                EAN_8,
+                EAN_13,
+                ITF,
+                MAXICODE,
+                PDF_417,
+                QR_CODE,
+                RSS_14,
+                RSS_EXPANDED,
+                UPC_A,
+                UPC_E,
+                All_1D,
+                UPC_EAN_EXTENSION,
+                MSI,
+                PLESSEY,
+                IMB,
+                PHARMA_CODE
+        }
+}
