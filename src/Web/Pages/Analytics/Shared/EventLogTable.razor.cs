0aadfd4d27bac2a74e8d78ff84029bfd34910568
Light4Code <light4code@gmail.com>
2023-01-25 10:52:28 +0100

    Extended Event Log by Service Type

--- src/Web/Pages/Analytics/Shared/EventLogTable.razor.cs
+++ src/Web/Pages/Analytics/Shared/EventLogTable.razor.cs
@@ -28,10 +28,10 @@ public partial class EventLogTable : ComponentBase
         try
         {
             var stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("Timestamp (UTC);Log Level;Service Name;Event;Event ID;Message");
+            stringBuilder.AppendLine("Timestamp (UTC);Log Level;Service Name;Service Type;Event;Event ID;Message");
             foreach (EventLogEntry entry in FilteredEntries)
             {
-                stringBuilder.AppendLine($"{entry.Timestamp};{entry.LogLevel};{entry.ServiceUniqueName};{entry.EventName};{entry.EventId};{entry.Message}");
+                stringBuilder.AppendLine($"{entry.Timestamp};{entry.LogLevel};{entry.ServiceUniqueName};{entry.ServiceType};{entry.EventName};{entry.EventId};{entry.Message}");
             }
 
             byte[] file = Encoding.UTF8.GetBytes(stringBuilder.ToString());
