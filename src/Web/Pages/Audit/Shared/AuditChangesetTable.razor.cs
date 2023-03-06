33b0f43d59c6f5729286f576165bcf9c7c6eed15
Light4Code <light4code@gmail.com>
2023-03-06 17:45:16 +0100

    Fixed changeset selection

--- src/Web/Pages/Audit/Shared/AuditChangesetTable.razor.cs
+++ src/Web/Pages/Audit/Shared/AuditChangesetTable.razor.cs
@@ -1,5 +1,6 @@
 using AyBorg.Web.Shared.Models;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace AyBorg.Web.Pages.Audit.Shared;
 
@@ -7,4 +8,11 @@ public partial class AuditChangesetTable : ComponentBase
 {
     [Parameter, EditorRequired] public IEnumerable<AuditChangeset>? Changesets { get; init; }
     public HashSet<AuditChangeset> SelectedChangesets { get; set; } = new();
+
+    private MudDataGrid<AuditChangeset> _grid = null!;
+
+    private void OnSelectedItemsChanged(HashSet<AuditChangeset> changesets)
+    {
+        SelectedChangesets = _grid.SelectedItems;
+    }
 }
