7347ea230be67815627198b0414db9fba0bbf2f6
Levent Tasdemir <light4code@gmail.com>
2023-07-26 19:36:33 +0200

    Test: Extended Mqtt-Client

--- tests/Plugins/MQTT/MqttClientTests.cs
+++ tests/Plugins/MQTT/MqttClientTests.cs
@@ -6,89 +6,142 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 
-namespace AyBorg.Plugins.MQTT.Tests
+namespace AyBorg.Plugins.MQTT.Tests;
+
+public class MqttClientTests
 {
-    public class MqttClientTests
+    private static readonly NullLogger<ICommunicationDevice> s_logger = new();
+    private readonly Mock<IMqttClientProviderFactory> _clientProviderFactoryMock = new();
+    private readonly Mock<IMqttClientProvider> _clientProviderMock = new();
+    private readonly Mock<ICommunicationStateProvider> _communicationStateProviderMock = new();
+
+    public MqttClientTests()
     {
-        private static readonly NullLogger<ICommunicationDevice> s_logger = new();
-        private readonly Mock<IMqttClientProviderFactory> _clientProviderFactoryMock = new();
-        private readonly Mock<IMqttClientProvider> _clientProviderMock = new();
-        private readonly Mock<ICommunicationStateProvider> _communicationStateProviderMock = new();
+        _clientProviderFactoryMock.Setup(f => f.Create(It.IsAny<ILogger>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()))
+            .Returns(_clientProviderMock.Object);
+        _communicationStateProviderMock.Setup(m => m.IsResultCommunicationEnabled).Returns(true);
+    }
 
-        public MqttClientTests() {
-            _clientProviderFactoryMock.Setup(f => f.Create(It.IsAny<ILogger>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()))
-                .Returns(_clientProviderMock.Object);
-            _communicationStateProviderMock.Setup(m => m.IsResultCommunicationEnabled).Returns(true);
-        }
+    [Fact]
+    public async Task TryConnectAsync_ShouldConnectToMqttBroker()
+    {
+        // Arrange
+        using var mqttClient = new MqttClient(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
 
-        [Fact]
-        public async Task TryConnectAsync_ShouldConnectToMqttBroker()
+        // Act
+        bool result = await mqttClient.TryConnectAsync();
+
+        // Assert
+        _clientProviderFactoryMock.Verify(f => f.Create(It.IsAny<ILogger>(), "MQTT Client (mqtt-client)", It.IsAny<string>(), It.IsAny<int>()), Times.Once);
+        _clientProviderMock.Verify(p => p.ConnectAsync(), Times.Once);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task TrySendAsync_ShouldPublishMqttMessage()
+    {
+        // Arrange
+        using var mqttClient = new MqttClient(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
+        string messageId = "test-message";
+        var portMock = new Mock<IPort>();
+
+        // Act
+        Assert.True(await mqttClient.TryConnectAsync());
+        bool result = await mqttClient.TrySendAsync(messageId, portMock.Object);
+
+        // Assert
+        _clientProviderMock.Verify(p => p.PublishAsync(messageId, portMock.Object, It.IsAny<MqttPublishOptions>()), Times.Once);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task SubscribeAsync_ShouldSubscribeToMqttTopic()
+    {
+        // Arrange
+        var rawSub = new MqttSubscription();
+        _clientProviderMock.Setup(m => m.SubscribeAsync(It.IsAny<string>())).ReturnsAsync(rawSub);
+        using var mqttClient = new MqttClient(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
+        string messageId = "test-message";
+
+        // Act
+        Assert.True(await mqttClient.TryConnectAsync());
+        IMessageSubscription subscription = await mqttClient.SubscribeAsync(messageId);
+
+        // Assert
+        _clientProviderMock.Verify(p => p.SubscribeAsync(messageId), Times.Once);
+        Assert.NotNull(subscription);
+    }
+
+    [Fact]
+    public async Task UnsubscribeAsync_ShouldUnsubscribeFromMqttTopic()
+    {
+        // Arrange
+        var rawSub = new MqttSubscription();
+        _clientProviderMock.Setup(m => m.SubscribeAsync(It.IsAny<string>())).ReturnsAsync(rawSub);
+        using var mqttClient = new MqttClient(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
+        string messageId = "test-message";
+        var subscriptionMock = new Mock<IMessageSubscription>();
+        subscriptionMock.Setup(m => m.Id).Returns(messageId);
+
+        // Act
+        Assert.True(await mqttClient.TryConnectAsync());
+        Assert.NotNull(await mqttClient.SubscribeAsync(messageId));
+        await mqttClient.UnsubscribeAsync(subscriptionMock.Object);
+
+        // Assert
+        _clientProviderMock.Verify(p => p.UnsubscribeAsync(It.IsAny<MqttSubscription>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Test_TryUpdateAsync()
+    {
+        // Arrange
+        using MqttClient plugin = new(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
+        var hostPort = (StringPort)plugin.Ports.First(p => p.Name.Equals("Host"));
+        hostPort.Value = "localhost";
+
+        var hostPortCopy = new StringPort(hostPort)
         {
-            // Arrange
-            using var mqttClient = new MqttClient(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
+            Value = "mqttbroker-1"
+        };
 
-            // Act
-            bool result = await mqttClient.TryConnectAsync();
+        // Act
+        bool result = await plugin.TryUpdateAsync(new List<IPort> { hostPortCopy });
 
-            // Assert
-            _clientProviderFactoryMock.Verify(f => f.Create(It.IsAny<ILogger>(), "MQTT Client (mqtt-client)", It.IsAny<string>(), It.IsAny<int>()), Times.Once);
-            _clientProviderMock.Verify(p => p.ConnectAsync(), Times.Once);
-            Assert.True(result);
-        }
+        // Assert
+        Assert.True(result);
+        Assert.Equal("mqttbroker-1", hostPort.Value);
+    }
 
-        [Fact]
-        public async Task TrySendAsync_ShouldPublishMqttMessage()
-        {
-            // Arrange
-            using var mqttClient = new MqttClient(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
-            string messageId = "test-message";
-            var portMock = new Mock<IPort>();
+    [Fact]
+    public async Task Test_TryConnectAsync()
+    {
+        // Arrange
+        using MqttClient plugin = new(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
+        var hostPort = (StringPort)plugin.Ports.First(p => p.Name.Equals("Host"));
+        hostPort.Value = "localhost";
 
-            // Act
-            Assert.True(await mqttClient.TryConnectAsync());
-            bool result = await mqttClient.TrySendAsync(messageId, portMock.Object);
+        // Act
+        bool result = await plugin.TryConnectAsync();
 
-            // Assert
-            _clientProviderMock.Verify(p => p.PublishAsync(messageId, portMock.Object, It.IsAny<MqttPublishOptions>()), Times.Once);
-            Assert.True(result);
-        }
+        // Assert
+        Assert.True(result);
+        Assert.True(plugin.IsConnected);
+    }
 
-        [Fact]
-        public async Task SubscribeAsync_ShouldSubscribeToMqttTopic()
-        {
-            // Arrange
-            var rawSub = new MqttSubscription();
-            _clientProviderMock.Setup(m => m.SubscribeAsync(It.IsAny<string>())).ReturnsAsync(rawSub);
-            using var mqttClient = new MqttClient(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
-            string messageId = "test-message";
+    [Fact]
+    public async Task Test_TryDisonnectAsync()
+    {
+        // Arrange
+        using MqttClient plugin = new(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
+        var hostPort = (StringPort)plugin.Ports.First(p => p.Name.Equals("Host"));
+        hostPort.Value = "localhost";
 
-            // Act
-            Assert.True(await mqttClient.TryConnectAsync());
-            IMessageSubscription subscription = await mqttClient.SubscribeAsync(messageId);
+        // Act
+        bool result = await plugin.TryDisconnectAsync();
 
-            // Assert
-            _clientProviderMock.Verify(p => p.SubscribeAsync(messageId), Times.Once);
-            Assert.NotNull(subscription);
-        }
-
-        [Fact]
-        public async Task UnsubscribeAsync_ShouldUnsubscribeFromMqttTopic()
-        {
-            // Arrange
-            var rawSub = new MqttSubscription();
-            _clientProviderMock.Setup(m => m.SubscribeAsync(It.IsAny<string>())).ReturnsAsync(rawSub);
-            using var mqttClient = new MqttClient(s_logger, _clientProviderFactoryMock.Object, _communicationStateProviderMock.Object, "mqtt-client");
-            string messageId = "test-message";
-            var subscriptionMock = new Mock<IMessageSubscription>();
-            subscriptionMock.Setup(m => m.Id).Returns(messageId);
-
-            // Act
-            Assert.True(await mqttClient.TryConnectAsync());
-            Assert.NotNull(await mqttClient.SubscribeAsync(messageId));
-            await mqttClient.UnsubscribeAsync(subscriptionMock.Object);
-
-            // Assert
-            _clientProviderMock.Verify(p => p.UnsubscribeAsync(It.IsAny<MqttSubscription>()), Times.Once);
-        }
+        // Assert
+        Assert.True(result);
+        Assert.False(plugin.IsConnected);
     }
 }
