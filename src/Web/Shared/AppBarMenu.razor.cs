d975dcb24bca90312756cc6453153f9012acc297
Light4Code <light4code@gmail.com>
2023-12-19 19:34:15 +0100

    Feature: App header navigation

---
+++ src/Web/Shared/AppBarMenu.razor.cs
@@ -0,0 +1,62 @@
+using AyBorg.SDK.System;
+using AyBorg.Web.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace AyBorg.Web.Shared;
+
+public partial class AppBarMenu : ComponentBase
+{
+    [Inject] ILogger<AppBarMenu> Logger { get; set; } = null!;
+    [Inject] IRegistryService RegistryService { get; set; } = null!;
+    [Inject] NavigationManager NavigationManager { get; set; } = null!;
+
+    private const string DASHBOARD_URI = "/dashboard";
+    private const string AGENTS_URI = "/agents/overview";
+    private const string AGENTS_BASE_URI = "/agents/";
+    private const string NET_URI = "/net/projects";
+    private const string NET_BASE_URI = "/net/";
+    private const string OBSERVABILITY_URI = "/observability/services";
+    private const string OBSERVABILITY_BASE_URI = "/observability/";
+    private const string AUDIT_URI = "/audit/reports";
+    private const string AUDIT_BASE_URI = "/audit/";
+
+    private bool _isDashboardAvailable = false;
+    private bool _isNetAvailable = false;
+    private bool _isAuditAvailable = false;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        NavigationManager.LocationChanged += (s, e) => Update();
+
+        try
+        {
+            IEnumerable<Models.ServiceInfoEntry> services = await RegistryService.ReceiveServicesAsync();
+            _isDashboardAvailable = services.Any(s => s.Type.Equals(ServiceTypes.Dashboard));
+            _isNetAvailable = services.Any(s => s.Type.Equals(ServiceTypes.Net));
+            _isAuditAvailable = services.Any(s => s.Type.Equals(ServiceTypes.Audit));
+        }
+        catch (Exception ex)
+        {
+            Logger.LogWarning(ex, "Could not retrieve analytics services from registry");
+        }
+    }
+
+    private string GetActiveClass(string page)
+    {
+        if (NavigationManager.Uri.Contains(page))
+        {
+            return "app-bar-nav-item mud-chip-text mud-chip-color-secondary ml-1 mr-1";
+        }
+        else
+        {
+            return "app-bar-nav-item ml-1 mr-1";
+        }
+    }
+
+    private void Update()
+    {
+        StateHasChanged();
+    }
+}
--- src/Web/Shared/NavMenu.razor.cs
+++ src/Web/Shared/NavMenu.razor.cs
@@ -9,18 +9,29 @@ public partial class NavMenu : ComponentBase
     [Inject] ILogger<NavMenu> Logger { get; set; } = null!;
     [Inject] IStateService StateService { get; set; } = null!;
     [Inject] IRegistryService RegistryService { get; set; } = null!;
+    [Inject] NavigationManager NavigationManager { get; set; } = null!;
 
-private bool _isDashboardAvailable = false;
+    private bool _isDashboardAvailable = false;
     private bool _isAnyAgentAvailable = false;
     private bool _isNetAvailable = false;
     private bool _isAnalyticsAvailable = false;
     private bool _isAuditAvailable = false;
 
-    private string _proVersion = "PRO";
+    private const string PRO_VERSION = " - PRO";
+
+    private string _dashboardTitle = "Dashboard";
+    private string _netTitle = "Artificial Intelligence";
+    private string _auditTitle = "Audit";
+
+    private Category _category = Category.None;
+
 
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
+
+        UpdateCategory();
+        NavigationManager.LocationChanged += (s, e) => OnUpdate();
 
         try
         {
@@ -31,6 +42,21 @@ private bool _isDashboardAvailable = false;
             _isNetAvailable = services.Any(s => s.Type.Equals(ServiceTypes.Net));
             _isAnalyticsAvailable = services.Any(s => s.Type.Equals(ServiceTypes.Log));
             _isAuditAvailable = services.Any(s => s.Type.Equals(ServiceTypes.Audit));
+
+            if(!_isDashboardAvailable)
+            {
+                _dashboardTitle = $"{_dashboardTitle}{PRO_VERSION}";
+            }
+
+            if(!_isNetAvailable)
+            {
+                _netTitle = $"{_netTitle}{PRO_VERSION}";
+            }
+
+            if(!_isAuditAvailable)
+            {
+                _auditTitle = $"{_auditTitle}{PRO_VERSION}";
+            }
         }
         catch (Exception ex)
         {
@@ -50,6 +76,60 @@ private bool _isDashboardAvailable = false;
 
     private async void OnUpdate()
     {
+        UpdateCategory();
         await InvokeAsync(StateHasChanged);
+    }
+
+    private void UpdateCategory()
+    {
+        string uri = NavigationManager.Uri;
+        if(uri.Contains("/dashboard"))
+        {
+            _category = Category.Dashboard;
+            return;
+        }
+
+        if(uri.Contains("/agents/"))
+        {
+            _category = Category.Agents;
+            return;
+        }
+
+        if(uri.Contains("/net/"))
+        {
+            _category = Category.Net;
+            return;
+        }
+
+        if(uri.Contains("/observability/"))
+        {
+            _category = Category.Observability;
+            return;
+        }
+
+        if(uri.Contains("/audit/"))
+        {
+            _category = Category.Audit;
+            return;
+        }
+
+        if(uri.Contains("/admin/"))
+        {
+            _category = Category.Admin;
+            return;
+        }
+
+        _category = Category.None;
+    }
+
+    private enum Category 
+    {
+        None,
+        Dashboard,
+        Agents,
+        Net,
+        Observability,
+        Audit,
+        Admin
     }
 }
