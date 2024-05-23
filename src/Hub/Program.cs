792260815c8a24b7201673dfb49c8d21bda694ab
Levent Tasdemir <light4code@gmail.com>
2024-05-23 21:05:48 +0200

    Hub connection WIP

--- src/Hub/Program.cs
+++ src/Hub/Program.cs
@@ -1,16 +1,20 @@
 using AyBorg.Hub.Connect;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication();
 builder.Services.AddGrpc();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
+app.UseAuthorization();
 // Configure the HTTP request pipeline.
 app.AddAyBorgAgentServer();
+app.UseForwardedHeaders();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
